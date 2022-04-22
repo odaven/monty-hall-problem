3d8f09c615394bb686f3b5b06f782e212577cd1d
odaven <18171281+odaven@users.noreply.github.com>
2022-04-23 00:14:41 +0100

    Game with change strategy

--- src/game_change_strategy.py
+++ src/game_change_strategy.py
@@ -1,3 +1,8 @@
+from src.contestant import Contestant
+from src.doors_setup import DoorsSetup
+from src.host import Host
+
+
 def game_change_strategy() -> bool:
     """
     Runs the change strategy
@@ -6,4 +11,15 @@ def game_change_strategy() -> bool:
     2 - Host opens a non-winning door
     3 - Contestant opens the remaining door
 
+    No need to now about the first choice here really
     """
+
+    doors_setup = DoorsSetup()
+    contestant = Contestant()
+
+    host = Host()
+    door_by_host = host.door(contestant, doors_setup)
+
+    second_choice = contestant.second_choice(door_by_host)
+
+    return doors_setup.doors[second_choice].winner
--- test/test_game_change_strategy.py
+++ test/test_game_change_strategy.py
@@ -7,13 +7,39 @@ from test.test_common import doors_setup_winner_in_position
 
 class TestGameChangeStrategy(TestCase):
 
+    @patch('src.game_change_strategy.Host')
     @patch('src.game_change_strategy.Contestant')
     @patch('src.game_change_strategy.DoorsSetup')
-    def test_game_change_selection_win(self, mocked_door_setup, mocked_contestant):
+    def test_game_change_selection_combinations(self, mocked_door_setup, mocked_contestant, mocked_host):
 
-        mocked_door_setup.return_value = doors_setup_winner_in_position(0)
-        mocked_contestant.return_value.first_choice = 0
+        combinations = [
+            {'winner_door': 0, 'host_door': 1, 'second_choice': 0, 'result': True},
+            {'winner_door': 0, 'host_door': 1, 'second_choice': 2, 'result': False},
 
-        result = game_change_strategy()
+            {'winner_door': 0, 'host_door': 2, 'second_choice': 0, 'result': True},
+            {'winner_door': 0, 'host_door': 2, 'second_choice': 1, 'result': False},
 
-        self.assertTrue(result)
+            {'winner_door': 1, 'host_door': 0, 'second_choice': 1, 'result': True},
+            {'winner_door': 1, 'host_door': 0, 'second_choice': 2, 'result': False},
+
+            {'winner_door': 1, 'host_door': 2, 'second_choice': 0, 'result': False},
+            {'winner_door': 1, 'host_door': 2, 'second_choice': 1, 'result': True},
+
+            {'winner_door': 2, 'host_door': 0, 'second_choice': 1, 'result': False},
+            {'winner_door': 2, 'host_door': 0, 'second_choice': 2, 'result': True},
+
+            {'winner_door': 2, 'host_door': 1, 'second_choice': 0, 'result': False},
+            {'winner_door': 2, 'host_door': 1, 'second_choice': 2, 'result': True}
+        ]
+
+        for combination in combinations:
+            winner_position = combination.get('winner_door')
+            mocked_door_setup.return_value = doors_setup_winner_in_position(winner_position)
+
+            mocked_host.return_value.door.return_value = combination.get('host_door')
+
+            mocked_contestant.return_value.second_choice.return_value = combination.get('second_choice')
+
+            result = game_change_strategy()
+
+            self.assertEqual(combination.get('result'), result, f'Failed with: {combination}')
