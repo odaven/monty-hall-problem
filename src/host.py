e7d004275393d44eff3282a9816c8a61fc1ccbde
odaven <18171281+odaven@users.noreply.github.com>
2022-04-22 23:13:30 +0100

    The host

---
+++ src/host.py
@@ -0,0 +1,12 @@
+from src.contestant import Contestant
+from src.doors_setup import DoorsSetup
+
+
+class Host:
+    """
+    The host will return the other not winning door
+    """
+    def door(self, contestant: Contestant, doors_setup: DoorsSetup) -> int:
+        for index, door in enumerate(doors_setup.doors):
+            if not door.winner and contestant.first_choice != index:
+                return index
---
+++ test/test_host.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+
+from src.contestant import Contestant
+from src.host import Host
+from test.test_common import doors_setup_winner_in_position
+
+
+class TestHost(TestCase):
+
+    def test_host_returns_door_0(self):
+
+        doors_setup = doors_setup_winner_in_position(0)
+
+        contestant = Contestant()
+        contestant.first_choice = 1
+
+        host = Host()
+
+        door_by_host = host.door(contestant, doors_setup)
+        self.assertEqual(2, door_by_host)
+
+    def test_combinations(self):
+        combinations = [
+            {'constestant_choice': 0, 'winner_door': 1, 'host_door': 2},
+            {'constestant_choice': 0, 'winner_door': 2, 'host_door': 1},
+
+            {'constestant_choice': 1, 'winner_door': 0, 'host_door': 2},
+            {'constestant_choice': 1, 'winner_door': 2, 'host_door': 0},
+
+            {'constestant_choice': 2, 'winner_door': 0, 'host_door': 1},
+            {'constestant_choice': 2, 'winner_door': 1, 'host_door': 0}
+        ]
+
+        for combination in combinations:
+
+            winner_position = combination.get('winner_door')
+            doors_setup = doors_setup_winner_in_position(winner_position)
+
+            contestant = Contestant()
+            contestant.first_choice = combination.get('constestant_choice')
+
+            door_by_host = Host().door(contestant, doors_setup)
+            self.assertEqual(combination.get('host_door'), door_by_host, f'Failed with: {combination}')
